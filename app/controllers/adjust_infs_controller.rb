7d63e01746cc4095f231c9d5c47a1bcf23a2805e
michelvalenzuela <87387556+michelvalenzuela@users.noreply.github.com>
2021-12-29 11:16:56 -0300

    api terminada.

--- app/controllers/adjust_infs_controller.rb
+++ app/controllers/adjust_infs_controller.rb
@@ -13,16 +13,13 @@ class AdjustInfsController < ApplicationController
       if not @adjust_inf.nil?
         if not @adjust_inf.errors.any?
           @client.update_attribute(:requests, @client.requests + 1)
-
           render json: { day: @adjust_inf.day, price: @adjust_inf.price, user_requests: @client.requests }
         else
           render json: @adjust_inf.errors
         end
-
       else
         render json: {status: "No Encontrado.."}
       end
-
     else
       render json: {status: "Añadir key de user.."}
     end
